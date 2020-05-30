a2d90dcc2a24b27a766ef6cc04e4826c9538c314
Edo Skaljo <eskaljo1@etf.unsa.ba>
2020-05-30 15:18:28 +0200

    logika

---
+++ AR sah/Assets/Scripts/BlackFigure.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackFigure : MonoBehaviour
+{
+    public static bool attacking = false;
+    public bool pawn;
+    private GameObject board;
+    public GameObject queenPrefab;
+
+    void Start()
+    {
+        attacking = false;
+        board = GameObject.FindGameObjectWithTag("B");
+    }
+
+    void Update()
+    {
+        if (pawn && transform.position.x > 5.0f)
+        {
+            GameObject g = Instantiate(queenPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.13f), Quaternion.identity);
+            g.transform.SetParent(board.transform);
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "White" && WhiteFigure.attacking)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ AR sah/Assets/Scripts/MoveFigure.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFigure : MonoBehaviour
+{
+    private GameObject touchedObject;
+    private GameObject figure;
+    private bool figureIsTouched = false;
+    private bool white = false;
+    private Material previousMat;
+    public Material selectedMat;
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                touchedObject = hit.transform.gameObject;
+                if (touchedObject.tag == "White" || touchedObject.tag == "Black")
+                {
+                    if (touchedObject.tag == "White")
+                    {
+                        if (!figureIsTouched)
+                        {
+                            white = true;
+                            WhiteFigure.attacking = true;
+                            BlackFigure.attacking = false;
+                            figure = touchedObject;
+                            figureIsTouched = true;
+                            previousMat = figure.GetComponent<MeshRenderer>().material;
+                            selectedMat.mainTexture = previousMat.mainTexture;
+                            figure.GetComponent<MeshRenderer>().material = selectedMat;
+                        }
+                        else
+                        {
+                            figure.GetComponent<MeshRenderer>().material = previousMat;
+                            figure.transform.position = new Vector3(touchedObject.transform.position.x, touchedObject.transform.position.y, figure.transform.position.z);
+                            figure = null;
+                            figureIsTouched = false;
+                        }
+                    }
+                    else
+                    {
+                        if (!figureIsTouched)
+                        {
+                            white = false;
+                            WhiteFigure.attacking = false;
+                            BlackFigure.attacking = true;
+                            figure = touchedObject;
+                            figureIsTouched = true;
+                            previousMat = figure.GetComponent<MeshRenderer>().material;
+                            selectedMat.mainTexture = previousMat.mainTexture;
+                            figure.GetComponent<MeshRenderer>().material = selectedMat;
+                        }
+                        else
+                        {
+                            figure.GetComponent<MeshRenderer>().material = previousMat;
+                            figure.transform.position = new Vector3(touchedObject.transform.position.x, touchedObject.transform.position.y, figure.transform.position.z);
+                            figure = null;
+                            figureIsTouched = false;
+                        }
+                    }
+                }
+                else if (touchedObject.tag == "Board" && figureIsTouched)
+                {
+                    figure.GetComponent<MeshRenderer>().material = previousMat;
+                    figure.transform.position = new Vector3(touchedObject.transform.position.x, touchedObject.transform.position.y, figure.transform.position.z);
+                    figure = null;
+                    figureIsTouched = false;
+                }
+            }
+        }
+    }
+}
