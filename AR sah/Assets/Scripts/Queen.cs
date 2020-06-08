57fc4830b46539955a84ab50a312a3af6eb73995
Edo Skaljo <eskaljo1@etf.unsa.ba>
2020-06-08 16:40:09 +0200

    dodani particle efekti za kraljicu

---
+++ AR sah/Assets/Scripts/Queen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Queen : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(TurnOffParticles());
+    }
+
+    IEnumerator TurnOffParticles()
+    {
+        yield return new WaitForSeconds(1.0f);
+        gameObject.GetComponentInChildren<ParticleSystem>().Stop();
+    }
+}
